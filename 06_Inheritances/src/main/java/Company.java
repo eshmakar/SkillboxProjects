a755ad5d036851717adc2e53ae78c493d3a8aea3
eshmakar <59125020+eshmakar@users.noreply.github.com>
2022-03-28 21:49:09 +0300

    OOP

--- 06_Inheritances/src/main/java/Company.java
+++ 06_Inheritances/src/main/java/Company.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Company {
 
     private String name;
-    private List<Employee> employeeList = new ArrayList<>();
+    private List<Employee> employeList = new ArrayList<>();
     private long income;
 
     public Company(String name) {
@@ -19,21 +19,21 @@ public class Company {
     }
 
     public List<Employee> getEmployeeList() {
-        return employeeList;
+        return employeList;
     }
 
     public void hire(EmployeeType employeeType) {
-        employeeList.add(EmployeeFactory.createEmployee(this, employeeType));
+        employeList.add(EmployeeFactory.createEmployee(this, employeeType));
     }
 
     public void hireAll(EmployeeType employeeType, int employeeCount) {
         for (int i = 0; i < employeeCount; i++) {
-            employeeList.add(EmployeeFactory.createEmployee(this, employeeType));
+            employeList.add(EmployeeFactory.createEmployee(this, employeeType));
         }
     }
 
     public void fire(int index) {
-        employeeList.remove(index);
+        employeList.remove(index);
     }
 
     public long getIncome() {
@@ -42,7 +42,7 @@ public class Company {
 
     public void setIncome() {
         income = 0;
-        income = employeeList
+        income = employeList
                 .stream()
                 .filter(e -> e instanceof Manager)
                 .map(e -> ((Manager) e).getEarningsForCompany())
@@ -50,7 +50,7 @@ public class Company {
     }
 
     private boolean isErrorStaffCount(int count) {
-        if (count < 0 || count > employeeList.size()) {
+        if (count < 0 || count > employeList.size()) {
             System.out.println("Количество сотрудников превышает фактическое или меньше нуля!");
             return true;
         }
@@ -59,14 +59,14 @@ public class Company {
 
     public List<Employee> getTopSalaryStaff(int count) {
         if (isErrorStaffCount(count)) count = 0;
-        List<Employee> topSalaryStaff = new ArrayList<>(employeeList);
+        List<Employee> topSalaryStaff = new ArrayList<>(employeList);
         topSalaryStaff.sort(Comparator.comparing(Employee::getMonthSalary).reversed());
         return topSalaryStaff.subList(0, count);
     }
 
     public List<Employee> getLowestSalaryStaff(int count) {
         if (isErrorStaffCount(count)) count = 0;
-        List<Employee> lowestSalaryStaff = new ArrayList<>(employeeList);
+        List<Employee> lowestSalaryStaff = new ArrayList<>(employeList);
         lowestSalaryStaff.sort(Comparator.comparing(Employee::getMonthSalary));
         return lowestSalaryStaff.subList(0, count);
     }
---
+++ 07_OOP/src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    private static String staffFile = "07_OOP/src/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        ArrayList<Employe> staff = loadStaffFromFile();
+        staff.sort(Comparator.comparing(Employe::getSalary).reversed().thenComparing(Employe::getName));
+        staff.forEach(System.out::println);
+
+    }
+
+    private static ArrayList<Employe> loadStaffFromFile() {
+        ArrayList<Employe> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employe(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
